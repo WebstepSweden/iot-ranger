2fa20393baeb12a4996ca58ae9649038bf64fa97
Peter Purmonen <peter@purmonen.com>
2017-04-08 17:18:05 +0200

    Stuff

--- server/src/main/java/se/webstep/iotr/database/Database.java
+++ server/src/main/java/se/webstep/iotr/database/Database.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toSet;
 
 public class Database {
