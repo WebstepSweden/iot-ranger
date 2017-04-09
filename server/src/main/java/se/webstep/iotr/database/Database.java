3309698aa553e2219297a052622ed0c644651ca0
Peter Purmonen <peter@purmonen.com>
2017-04-09 09:13:36 +0200

    Supoort for delete location and added test data fixture

--- server/src/main/java/se/webstep/iotr/database/Database.java
+++ server/src/main/java/se/webstep/iotr/database/Database.java
@@ -16,11 +16,37 @@ public class Database {
     private Set<Registration> registrations;
 
 
+
     private Database() {
+
         this.locations = new HashSet<>();
         this.registrations = new HashSet<>();
+        insertTestData();
+
     }
 
+
+    private void insertTestData() {
+
+        addLocation("Lobby");
+        addLocation("Boiler Room");
+        addLocation("Parking");
+        addLocation("Tee 1");
+
+        String id = "206881543";
+        LocalDateTime now = LocalDateTime.now();
+        register(id, now, "Lobby");
+        register(id, now.minusSeconds(1), "Lobby");
+        register(id, now.minusSeconds(2), "Lobby");
+        register(id, now.minusSeconds(3), "Lobby");
+        register(id, now, "Boiler Room");
+        register(id, now.minusSeconds(3), "Boiler Room");
+        register(id, now, "Parking");
+        register(id, now.minusSeconds(4), "Parking");
+        register(id, now, "Tee 1");
+        register(id, now.minusMinutes(3).minusSeconds(12), "Tee 1");
+
+    }
 
 
 
